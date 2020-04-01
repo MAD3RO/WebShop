c1d6e2def870247f672b85a481c0b7f063944d3b
Marko Huljak <marko.huljak@gdi.net>
2020-04-01 14:33:34 +0200

    bundle organized, category update

--- WebShop/App_Start/BundleConfig.cs
+++ WebShop/App_Start/BundleConfig.cs
@@ -8,17 +8,13 @@ namespace WebShop
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-            "~/Scripts/jquery-{version}.js",
+            "~/Scripts/jquery-3.4.1.min.js",
             //"~/Scripts/popper.min.js",
             "~/Scripts/bootstrap.min.js",
+            "~/Scripts/jquery.validate.min.js",
+            "~/Scripts/jquery.validate.unobtrusive.min.js",
+            "~/Scripts/jquery.unobtrusive-ajax.min.js",
             "~/Scripts/slick.min.js",
             "~/Scripts/nouislider.min.js",
             "~/Scripts/jquery.zoom.min.js",
@@ -40,12 +36,8 @@ namespace WebShop
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
-                      //"~/Content/bootstrap-responsive.min.css",
                       "~/Content/slick.css",
                       "~/Content/slick-theme.css",
                       "~/Content/nouislider.min.css",
